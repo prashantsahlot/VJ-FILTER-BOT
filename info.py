7ba6a04f0db6c948c1882f5a776bab2f8e63e6e4
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2025-01-05 11:42:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Frozenmoviebot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutoFilter')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
